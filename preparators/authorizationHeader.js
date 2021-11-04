ba38117edf232447ebaa430d522ac376a1b1b2bd
phuhl <git@ph-uhl.com>
2021-11-04 10:04:06 +0100

    Fixes linting errors

--- preparators/authorizationHeader.js
+++ preparators/authorizationHeader.js
@@ -5,11 +5,11 @@
  * @returns {[string, string]} Username and Password
  */
 const basicAuth = (req) => {
-  let m = /^Basic\ (.*)$/.exec(req.get("Authorization") || "");
+  let m = /^Basic (.*)$/.exec(req.get("Authorization") || "");
   if (m) {
     m = /([^:]*):(.*)/.exec(new Buffer(m[1], "base64").toString("ascii"));
     if (m) {
-      let email = m[1],
+      const email = m[1],
         token = m[2];
       return [email, token];
     }
@@ -24,7 +24,7 @@ const basicAuth = (req) => {
  * @returns {[string, string]} Username and Password
  */
 const bearerAuth = (req) => {
-  let m = /^Bearer\ (.*)$/.exec(req.get("Authorization") || "");
+  const m = /^Bearer (.*)$/.exec(req.get("Authorization") || "");
   if (m) {
     return m[1];
   }
