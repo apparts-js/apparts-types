315bb90c3785cd5c78816a0bd1411b26dfe35208
phuhl <git@ph-uhl.com>
2022-08-23 10:15:24 +0200

    Adds recursiveCheck function

---
+++ src/types/recursiveCheck.test.js
@@ -0,0 +1,187 @@
+import { recursiveCheck } from "./recursiveCheck";
+const checkType = (a, b) => recursiveCheck(a, b) === true;
+
+import {
+  object,
+  time,
+  array,
+  email,
+  string,
+  bool,
+  base64,
+  hex,
+  float,
+  uuidv4,
+  int,
+  any,
+  value,
+} from "./tests/types";
+
+describe("recursiveCheck, explain wrong type", () => {
+  it("should explain wrong value", async () => {
+    expect(recursiveCheck({ value: "test" }, "Test")).toStrictEqual({
+      key: "",
+      shouldType: { value: "test" },
+      isValue: "Test",
+    });
+  });
+  it("should explain wrong type", async () => {
+    expect(recursiveCheck({ type: "int" }, 7.5)).toStrictEqual({
+      key: "",
+      shouldType: { type: "int" },
+      isValue: 7.5,
+    });
+  });
+  it("should explain wrong array type", async () => {
+    expect(
+      recursiveCheck({ type: "array", items: { type: "/" } }, 7.5)
+    ).toStrictEqual({
+      key: "",
+      shouldType: { type: "array", items: { type: "/" } },
+      isValue: 7.5,
+    });
+  });
+  it("should explain wrong object type", async () => {
+    expect(
+      recursiveCheck({ type: "object", keys: { test: { type: "/" } } }, 7.5)
+    ).toStrictEqual({
+      key: "",
+      shouldType: { type: "object", keys: { test: { type: "/" } } },
+      isValue: 7.5,
+    });
+  });
+  it("should explain wrong objectValue type", async () => {
+    expect(
+      recursiveCheck({ type: "object", values: { type: "/" } }, 7.5)
+    ).toStrictEqual({
+      key: "",
+      shouldType: { type: "object", values: { type: "/" } },
+      isValue: 7.5,
+    });
+  });
+});
+
+describe("recursiveCheck, explain obj wrong child", () => {
+  it("should explain too many children", async () => {
+    expect(
+      recursiveCheck(
+        { type: "object", keys: { test: { type: "/" } } },
+        { test: 1, other: 2 }
+      )
+    ).toStrictEqual({
+      key: "other",
+      shouldType: undefined,
+      isValue: 2,
+    });
+  });
+  it("should explain too few children", async () => {
+    expect(
+      recursiveCheck({ type: "object", keys: { test: { type: "string" } } }, {})
+    ).toStrictEqual({
+      key: "test",
+      shouldType: { type: "string" },
+      isValue: undefined,
+    });
+  });
+  it("should explain wrong children", async () => {
+    expect(
+      recursiveCheck(
+        { type: "object", keys: { test: { type: "int" } } },
+        { test: 6.6 }
+      )
+    ).toStrictEqual({
+      key: "test",
+      shouldType: { type: "int" },
+      isValue: 6.6,
+    });
+  });
+  it("should explain wrong child one level deep", async () => {
+    expect(
+      recursiveCheck(
+        {
+          type: "object",
+          keys: {
+            test: {
+              type: "object",
+              keys: {
+                deep: { type: "int" },
+              },
+            },
+          },
+        },
+        { test: {} }
+      )
+    ).toStrictEqual({
+      key: "test.deep",
+      shouldType: { type: "int" },
+      isValue: undefined,
+    });
+  });
+});
+
+// TODO:
+// - array children
+// - oneOf children
+// - objValue children
+
+describe("recursiveCheck should accept valid input", () => {
+  it("should correctly classify value", async () => {
+    value(checkType);
+  });
+
+  it("should correctly classify any", async () => {
+    any(checkType);
+  });
+
+  it("should correctly classify int", async () => {
+    int(checkType);
+  });
+  it("should correctly classify id", async () => {
+    int(checkType, "id");
+  });
+
+  it("should correctly classify uuid", async () => {
+    uuidv4(checkType);
+  });
+
+  it("should correctly classify float", async () => {
+    float(checkType);
+  });
+
+  it("should correctly classify hex", async () => {
+    hex(checkType);
+  });
+
+  it("should correctly classify base64", async () => {
+    base64(checkType);
+  });
+
+  it("should correctly classify bool", async () => {
+    bool(checkType, "bool");
+  });
+  it("should correctly classify boolean", async () => {
+    bool(checkType, "boolean");
+  });
+  it("should correctly classify string", async () => {
+    string(checkType);
+  });
+  it("should correctly classify password", async () => {
+    string(checkType, "password");
+  });
+
+  it("should correctly classify email", async () => {
+    email(checkType);
+  });
+
+  it("should correctly classify array", async () => {
+    array(checkType);
+  });
+
+  it("should correctly classify time", async () => {
+    time(checkType);
+  });
+
+  it("should correctly classify object", async () => {
+    object(checkType);
+  });
+});
---
+++ src/types/recursiveCheck.ts
@@ -0,0 +1,109 @@
+import { types as checkTypes } from "./types";
+import { Type } from "../schema";
+
+const unique = (list: string[]) => {
+  const set: Record<string, true> = {};
+  for (const item of list) {
+    set[item] = true;
+  }
+  return Object.keys(set);
+};
+
+type Issue = {
+  key: string;
+  shouldType: Type;
+  isValue: unknown;
+};
+
+export const recursiveCheck = (
+  type: Type | undefined,
+  response: unknown,
+  key = ""
+): true | Issue => {
+  if (!type) {
+    return { key, shouldType: type, isValue: response };
+  }
+
+  if ("value" in type) {
+    const matches = JSON.stringify(type.value) === JSON.stringify(response);
+    if (matches === true) {
+      return true;
+    }
+    return { key, shouldType: type, isValue: response };
+  }
+
+  if (type.type === "oneOf" && Array.isArray(type.alternatives)) {
+    return type.alternatives.reduce(
+      (a: true | Issue, b) =>
+        a === true ? a : recursiveCheck(b, response, key),
+      { key, shouldType: type, isValue: response }
+    );
+  }
+  if (type.type === "array" && checkTypes.array.check(response)) {
+    return (response as unknown[]).reduce(
+      (a: true | Issue, b, i) =>
+        a === true ? recursiveCheck(type.items, b, key + `[${i}]`) : a,
+      true
+    );
+  }
+  if (type.type === "object") {
+    const responseIsObject =
+      typeof response === "object" &&
+      !Array.isArray(response) &&
+      response !== null;
+    const hasKeys =
+      "keys" in type && typeof type.keys === "object" && type.keys !== null;
+    const hasValues =
+      "values" in type &&
+      typeof type.values === "object" &&
+      type.values !== null;
+
+    if (!responseIsObject || (!hasKeys && !hasValues)) {
+      return { key, shouldType: type, isValue: response };
+    }
+
+    if (hasKeys) {
+      const notOptionalAndNull = (key) =>
+        !(response[key] === null && type.keys[key] && type.keys[key].optional);
+
+      const allKeysToCheck = unique([
+        ...Object.keys(response),
+        ...Object.keys(type.keys).filter(notOptionalAndNull),
+      ]);
+
+      const matches = allKeysToCheck.reduce(
+        (a: true | Issue, b) =>
+          a === true
+            ? recursiveCheck(
+                type.keys[b],
+                response[b],
+                (key ? key + "." : "") + b
+              )
+            : a,
+        true
+      );
+      if (matches !== true) {
+        return matches;
+      }
+    }
+    if (hasValues) {
+      return Object.keys(response).reduce(
+        (a: true | Issue, b) =>
+          a === true ? recursiveCheck(type.values, response[b]) : a,
+        true
+      );
+    }
+  }
+  if (type.type) {
+    const matches =
+      checkTypes[type.type] && checkTypes[type.type].check(response);
+    if (matches) {
+      return true;
+    }
+  }
+  return { key, shouldType: type, isValue: response };
+};
+
+export const checkSchema = (response, schema) => {
+  return recursiveCheck(schema.getType(), response);
+};
