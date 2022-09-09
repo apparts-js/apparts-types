1d62566b83fe2e98b95b9e8ca09f900d196c7f8c
phuhl <git@ph-uhl.com>
2022-09-09 15:38:39 +0200

    Adds more tests to typeSchema

--- src/utils/types/typeSchema.test.js
+++ src/utils/types/typeSchema.test.js
@@ -1,8 +1,76 @@
 import { typeSchema } from "./typeSchema";
 import { rtype } from "./typeType";
+import { checkSchema } from "../../types/checkType";
+import { explainCheck } from "../../types/explainCheck";
 
 describe("typeSchema", () => {
   it("should match type with rtype", async () => {
     expect(typeSchema.getType()).toMatchObject(rtype);
+  });
+
+  it("should check type", async () => {
+    expect(
+      checkSchema(
+        {
+          type: "object",
+          keys: {
+            anArray: {
+              type: "array",
+              items: {
+                type: "string",
+              },
+            },
+          },
+        },
+        typeSchema
+      )
+    ).toBeTruthy();
+
+    expect(
+      checkSchema(
+        {
+          type: "object",
+          keys: {
+            anArray: {
+              type: "array",
+            },
+          },
+        },
+        typeSchema
+      )
+    ).toBeFalsy();
+  });
+
+  it("should explain check type", async () => {
+    expect(
+      explainCheck(
+        {
+          type: "object",
+          keys: {
+            anArray: {
+              type: "array",
+              items: {
+                type: "string",
+              },
+            },
+          },
+        },
+        typeSchema
+      )
+    ).toBeTruthy();
+
+    expect(
+      typeof explainCheck(
+        {
+          type: "object",
+          keys: {
+            anArray: {
+              type: "array",
+            },
+          },
+        },
+        typeSchema
+      )
+    ).toBe("string");
   });
 });
