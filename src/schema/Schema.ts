683663912972745043fd62fbd964361988897eb1
phuhl <git@ph-uhl.com>
2023-06-14 21:14:02 +0200

    Adds documentation

--- src/schema/Schema.ts
+++ src/schema/Schema.ts
@@ -1,12 +1,5 @@
-import {
-  Required,
-  Public,
-  FlagsType,
-  Type,
-  Derived,
-  _Optional,
-} from "./utilTypes";
 import { fillInDefaults } from "../utils/fillInDefaults";
+import { FlagsType, Type } from "./utilTypes";
 
 export abstract class Schema<
   Flags extends FlagsType,
@@ -14,12 +7,26 @@ export abstract class Schema<
   PublicType = SchemaType,
   NotDerivedType = SchemaType
 > {
+  // cloneWithType is defined in the non-abstract classes. It must not
+  // be used in this abstract class as it would here only return a
+  // Schema, not the correct instance class. In the non-abstract
+  // classes cloneWithType is overwritten in such a way that the
+  // instance class is returned. Hence, in the non-abstract classes it
+  // can (and has to be) used.
   abstract cloneWithType<Flags extends FlagsType>(
     type: Type
   ): Schema<Flags, SchemaType, PublicType, NotDerivedType>;
 
+  // Clone can be used here as it returns the instance type
+  // (this). This is possible as we do not need to change any flags.
   abstract clone(type: Type): this;
 
+  // Here are the functions that manipulate the type but not the
+  // flags. We can define them here as we have clone accessible in
+  // here. Functions like optional or derived can not be defined here
+  // as they need the cloneWithType function that we cannot correctly
+  // type here in a way that it returns instance classes instead of
+  // Schema.
   description(description: string) {
     return this.clone({ ...this.type, description });
   }
