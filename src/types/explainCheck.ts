e00e3a960d3af0b640a88aadaa40e059cfc236ed
phuhl <git@ph-uhl.com>
2023-07-23 13:54:08 +0200

    Improves explainCheck texts

--- src/types/explainCheck.ts
+++ src/types/explainCheck.ts
@@ -8,42 +8,43 @@ export const explainCheck = (response: unknown, type: Type) => {
   if (result === true) {
     return false;
   }
+  const resultKeyName = result.key ? result.key : "Value";
 
   const prettyValue = JSON.stringify(result.isValue, undefined, 2);
   if (!result.shouldType) {
-    return `❌ ➕ Too much: Found '${result.key}' with value ${prettyValue}. Should not be there.`;
+    return `❌ ➕ Too much: Found '${resultKeyName}' with value ${prettyValue}. Should not be there.`;
   }
   if (
     (result.isValue === null || result.isValue === undefined) &&
     result.shouldType
   ) {
-    return `❌ ➖ Missing: '${result.key}' is not present.`;
+    return `❌ ➖ Missing: '${resultKeyName}' is not present.`;
   }
 
   if ("value" in result.shouldType) {
-    return `❌ Value wrong: '${result.key}' should be '${result.shouldType.value}', but got '${response}'`;
+    return `❌ Value wrong: '${resultKeyName}' should be '${result.shouldType.value}', but got '${response}'`;
   }
   if (result.shouldType.type === "object") {
     if (!("keys" in result.shouldType || "values" in result.shouldType)) {
-      return `❌ Invalid Type: '${result.key}' with type '${result.shouldType}' has neither values nor keys.`;
+      return `❌ Invalid Type: '${resultKeyName}' with type '${result.shouldType}' has neither values nor keys.`;
     }
   }
 
   if (result.shouldType.type === "oneOf") {
     if (!("keys" in result.shouldType || "values" in result.shouldType)) {
-      return `❌ No alternative matches: '${result.key}' is ${prettyValue} but does not match any type of oneOf.`;
+      return `❌ No alternative matches: '${resultKeyName}' is ${prettyValue} but does not match any type of oneOf.`;
     }
   }
 
   if (!checkTypes[result.shouldType.type]) {
-    return `❌ Unkown type: '${result.key}' has unknown type '${result.shouldType.type}'`;
+    return `❌ Unkown type: '${resultKeyName}' has unknown type '${result.shouldType.type}'`;
   }
 
   if (result.reason) {
-    return `❌ Custom check failed: '${result.key}' ${result.reason}`;
+    return `❌ Custom check failed: '${resultKeyName}' ${result.reason}`;
   }
 
-  return `❌ Type missmatch: '${result.key}' is ${prettyValue} but should be of type '${result.shouldType}'`;
+  return `❌ Type missmatch: '${resultKeyName}' is ${prettyValue} but should be of type '${result.shouldType}'`;
 };
 
 export const explainSchemaCheck = (response, schema) => {
